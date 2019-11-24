32aaf5e9b073205449ff2c41e312980900687f21
Jean-Philippe Meuret <jean-philippe.meuret@michelin.com>
2019-11-24 16:26:48 +0100

    AutoDS Explicitation générique specs analyses utilisateur (multi-onglets, impl/expl)

--- autods.py
+++ autods.py
@@ -1979,10 +1979,11 @@ def addTransectEffort(dfInSights, transectCol, passesCol):
     return dfInSights.join(dfPointEffort, on='Point')
     
 
-# Generation of a table of implicit variant specification,
+# Generation of a table of implicit "partial variant" specification,
 # from a list of possible data selection criteria for each variable.
+# "Partial variant" because its only about a sub-set of variants
 # * dVariants : { target columns name: list of possibles criteria for data selection }
-def implicitCombinationSpec(dVariants):
+def implicitPartialVariantSpecs(dVariants):
     
     def fixedLengthList(toComplete, length):
         return toComplete + [np.nan]*(length - len(toComplete))
@@ -1991,8 +1992,9 @@ def implicitCombinationSpec(dVariants):
 
     return pd.DataFrame({ colName : fixedLengthList(variants, nRows) for colName, variants in dVariants.items() })
 
-# Generation of a table of explicit variant specifications, from an implicit one
-def explicitCombinationSpec(dfImplSpecs):
+# Generation of a table of explicit "partial variant" specifications, from an implicit one
+# (= generate all combinations of variants)
+def explicitPartialVariantSpecs(dfImplSpecs):
     
     dfExplSpecs = dfImplSpecs[dfImplSpecs.columns[:1]].dropna()
     
@@ -2011,6 +2013,73 @@ def explicitCombinationSpec(dfImplSpecs):
         # Reset index for easy next duplication
         dfExplSpecs.reset_index(inplace=True, drop=True)
 
+    # Done.
+    return dfExplSpecs
+
+# Generation of a table of explicit variant specifications,
+# from a set of implicit and explicit partial variant specs tables
+# * oddfPartSpecs : the odict of name => partial specs table
+#   Warning: implicit tables are only found by their name containing "_impl"
+def explicitVariantSpecs(oddfPartSpecs):
+    
+    assert len(oddfPartSpecs.keys()) > 0, "Error: Can't explicit variants with no partial variant"
+    
+    # Group partial specs tables with same column sets (according to column names)
+    odSameColsPsNames = odict() # { sorted(cols): [table names] }
+    
+    for psName, dfPsValues in oddfPartSpecs.items():
+        
+        colSetId = ':'.join(sorted(dfPsValues.columns))
+        if colSetId not in odSameColsPsNames:
+            odSameColsPsNames[colSetId] = list()
+            
+        odSameColsPsNames[colSetId].append(psName)
+
+    # For each group, concat. tables into one, after expliciting if needed
+    ldfExplPartSpecs = list()
+
+    for lPsNames in odSameColsPsNames.values():
+
+        ldfSameColsPartSpecs= list()
+        for psName in lPsNames:
+
+            dfPartSpecs = oddfPartSpecs[psName]
+
+            # Implicit specs case:
+            if '_impl' in psName:
+
+                dfPartSpecs = explicitPartialVariantSpecs(dfPartSpecs)
+
+            # Now, specs are explicit.
+            ldfSameColsPartSpecs.append(dfPartSpecs)
+
+        # Concat groups of same columns set explicit specs
+        ldfExplPartSpecs.append(pd.concat(ldfSameColsPartSpecs, ignore_index=True))
+    
+    # Combinaison des specs explicites (dans l'ordre)
+    dfExplSpecs = ldfExplPartSpecs[0]
+
+    for dfExplPartSpecs in ldfExplPartSpecs[1:]:
+
+        commCols = [col for col in dfExplSpecs.columns if col in dfExplPartSpecs.columns]
+
+        if commCols: # Any column in common : left join each left row to each matching right row
+
+            dfExplSpecs = dfExplSpecs.join(dfExplPartSpecs.set_index(commCols), on=commCols)
+
+        else: # No columns in common : combine each left row with all right rows
+
+            nInitSpecs = len(dfExplSpecs)
+
+            dfExplSpecs = dfExplSpecs.loc[np.repeat(dfExplSpecs.index, len(dfExplPartSpecs))]
+            dfExplSpecs.reset_index(drop=True, inplace=True)
+
+            dfExplPartSpecs = pd.DataFrame(data=np.tile(dfExplPartSpecs, [nInitSpecs, 1]), columns=dfExplPartSpecs.columns)
+
+            dfExplSpecs = pd.concat([dfExplSpecs, dfExplPartSpecs], axis='columns')
+
+        dfExplSpecs.reset_index(drop=True, inplace=True)
+    
     # Done.
     return dfExplSpecs
 
@@ -2076,7 +2145,7 @@ def addSurveyAreaInfo(dfInSights, dAreaInfo):
     for col, values in dAreaInfo.items():
         dfInSights[col] = values
         
-    return dInSights
+    return dfInSights
     
 
 if __name__ == '__main__':
